6e5cbf3aa478633b0461ae674c223a05ecbe0cd1
dionne0316 <avellanadionne20@gmail.com>
2024-10-04 09:17:31 +0800

    Add files via upload

---
+++ CC102_02BasicOutput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+		cout << "Our Lady of Lourdes College Foundation"<<endl;
+		cout << "College of Computer Studies"<<endl;
+		cout << "CS-1A"<<endl;
+		cout << "Dionne A. Avellana"<<endl;
+		cout << "P-2 Parang, Jose Panganiban Camarines Norte"<<endl;
+		
+		return 0;
+}
